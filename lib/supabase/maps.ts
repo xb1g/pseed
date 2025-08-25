924d98dd515d1df790e8b3321981944e5b47b2c5
Solnar <49712045+Ongpaoaa@users.noreply.github.com>
2025-08-25 11:41:50 +0700

    grading

--- lib/supabase/maps.ts
+++ lib/supabase/maps.ts
@@ -171,11 +171,12 @@ export const getMapsWithStats = async (): Promise<
       .eq("user_id", user.id);
 
     if (classroomMemberships) {
-      userClassrooms = classroomMemberships.flatMap((m) =>
-        ((m.classrooms as any).classroom_maps as any[])?.map((cm: any) => ({
-          map_id: cm.map_id,
-          classroom_name: (m.classrooms as any).name,
-        })) || []
+      userClassrooms = classroomMemberships.flatMap(
+        (m: any) =>
+          m.classrooms.classroom_maps?.map((cm: any) => ({
+            map_id: cm.map_id,
+            classroom_name: m.classrooms.name,
+          })) || []
       );
     }
 
